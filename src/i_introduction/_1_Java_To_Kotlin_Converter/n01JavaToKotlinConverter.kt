c4810c3551cb7efea4bf232d2ff5db7ccc08aab5
Marc Villagrasa <marcvillagrasa@Marcs-MacBook-Pro.local>
2019-02-15 10:51:00 +0100

    Resolve task1

--- src/i_introduction/_1_Java_To_Kotlin_Converter/n01JavaToKotlinConverter.kt
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/n01JavaToKotlinConverter.kt
@@ -12,7 +12,7 @@ fun todoTask1(collection: Collection<Int>): Nothing = TODO(
     """,
     references = { JavaCode1().task1(collection) })
 
-
 fun task1(collection: Collection<Int>): String {
-    todoTask1(collection)
+    return collection.joinToString(", ", "{", "}", transform = Int::toString)
+
 }
