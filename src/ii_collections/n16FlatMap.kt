761a6f166b1f46461dc5ac7dc063195a833eea98
Marc Villagrasa <marcvillagrasa@Marcs-MacBook-Pro.local>
2019-02-16 10:37:00 +0100

    Resolve task16

--- src/ii_collections/n16FlatMap.kt
+++ src/ii_collections/n16FlatMap.kt
@@ -1,5 +1,6 @@
 package ii_collections
 
+
 fun example() {
 
     val result = listOf("abc", "12").flatMap { it.toList() }
@@ -7,12 +8,12 @@ fun example() {
     result == listOf('a', 'b', 'c', '1', '2')
 }
 
-val Customer.orderedProducts: Set<Product> get() {
-    // Return all products this customer has ordered
-    todoCollectionTask()
-}
+val Customer.orderedProducts: Set<Product> get() =
+    orders
+        .flatMap{ it.products}
+        .toSet()
 
-val Shop.allOrderedProducts: Set<Product> get() {
-    // Return all products that were ordered by at least one customer
-    todoCollectionTask()
-}
+val Shop.allOrderedProducts: Set<Product> get() =
+    customers
+        .flatMap { it.orderedProducts }
+        .toSet()
